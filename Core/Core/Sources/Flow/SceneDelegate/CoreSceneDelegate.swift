cd1aa3c41992d2581a6697630a5206ed3629238e
Samuel <samuelbsantosn@gmail.com>
2023-05-26 12:04:33 -0300

    feat: Creating customizable scene delegate

---
+++ Core/Core/Sources/Flow/SceneDelegate/CoreSceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  CoreSceneDelegate.swift
+//  Core
+//
+//  Created by Samuel Brasileiro on 26/05/23.
+//
+
+import UIKit
+
+open class CoreSceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    public var window: UIWindow?
+    
+    public var coordinator: (any Coordinator)?
+    
+    public func scene(_ scene: UIScene,
+                      willConnectTo session: UISceneSession,
+                      options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        
+        setupEnvironment()
+        window.rootViewController = coordinator?.navigationController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+    
+    open func setupEnvironment() { }
+}
