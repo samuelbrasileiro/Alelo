f0646b29c33895568ea9ee8bbc6b43adb1c0ee8b
Samuel <samuelbsantosn@gmail.com>
2023-05-26 08:03:19 -0300

    feat: Creating Service Tests Module

---
+++ Service/ServiceTests/Tests/NetworkServiceTests.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkServiceTests.swift
+//  ServiceTests
+//
+//  Created by Samuel Brasileiro on 29/10/22.
+//
+
+import XCTest
+@testable import Service
+
+final class NetworkServiceTests: XCTestCase {
+    
+    var networkService: NetworkServiceProtocol!
+    
+}
