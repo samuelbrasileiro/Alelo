e6af7e115fab9b371e57bfdb21bd8c2404483aa8
Samuel <samuelbsantosn@gmail.com>
2023-05-26 12:00:25 -0300

    feat: Implementing the flow coordinator of Module Store

---
+++ Store/Store/Sources/UI/Flows/StoreCoordinator.swift
@@ -0,0 +1,56 @@
+//
+//  StoreCoordinator.swift
+//  Store
+//
+//  Created by Samuel Brasileiro on 26/05/23.
+//
+
+import Core
+import DependencyInjection
+import UIKit
+
+public protocol StoreCoordinatorDelegate: AnyObject { }
+
+public final class StoreCoordinator: CoreCoordinator {
+    
+    public enum Route: RouteProtocol {
+        case productDetails
+        case bestSellers
+        case cart
+    }
+    
+    // MARK: - PUBLIC ATTRIBUTES
+    
+    public var navigationController: UINavigationController
+    weak public var parentCoordinator: (any Coordinator)?
+    public var childCoordinators: [any Coordinator] = []
+    
+    public weak var delegate: StoreCoordinatorDelegate?
+    let resolver: DependencyResolver
+    
+    // MARK: - INITIALIZER
+    
+    public required init(resolver: DependencyResolver,
+                         navigationController: UINavigationController) {
+        self.resolver = resolver
+        self.navigationController = navigationController
+    }
+    
+    // MARK: - PUBLIC METHODS
+    
+    public func start() { 
+        startToRoute()
+    }
+    
+    public func startToRoute(_ route: Route? = nil) { 
+        navigate(to: route ?? .bestSellers)
+    }
+    
+    public func getTransition(to route: Route) -> UIViewController? {
+        switch route {
+        case .bestSellers: return UIViewController()
+        case .cart: return UIViewController()
+        case .productDetails: return UIViewController()
+        }
+    }
+}
