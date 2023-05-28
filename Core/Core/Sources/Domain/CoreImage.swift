337768641aa45bfc78666f76b508d1398d139295
Samuel <samuelbsantosn@gmail.com>
2023-05-28 10:37:12 -0300

    refactor: Changing CoreImage to enum

--- Core/Core/Sources/Domain/CoreImage.swift
+++ Core/Core/Sources/Domain/CoreImage.swift
@@ -7,24 +7,15 @@
 
 import UIKit
 
-public class CoreImage {
-    
-    public enum Name {
-        case unavailableImage
-    }
+public enum CoreImage {
+    case unavailableImage
     
     public var image: UIImage {
         getImageAsset().image
     }
     
-    private let name: CoreImage.Name
-    
-    public init(named name: CoreImage.Name) {
-        self.name = name
-    }
-    
     private func getImageAsset() -> ImageAsset {
-        switch name {
+        switch self {
         case .unavailableImage: return Asset.unavailableImage
         }
     }
